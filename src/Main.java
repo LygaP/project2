59b2221ecb273717da6ce722218e83b630ae5cb6
LygaP <liga.paleckiene@gmail.com>
2023-02-15 19:53:18 +0200

    nothng important2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -7,7 +9,7 @@ public class Main {
         // susikurti kintamajį int tipo, ir jame išsaugoti dabartinius metus (2023) ir atspausdinti.
         // sugeneruoti sveikajį atsitiktinį skaičių nuo 0 iki 50 intervale bei jį atspausdinti
         // sugeneruoti sveikajį atsitiktinį skaičių nuo 20 iki 70 intervale bei jį atspausdinti
-        System.out.println("=============== uzd 1. =================");
+
 
         String city = "Kaunas";
         System.out.println(city);
@@ -30,8 +32,9 @@ public class Main {
         int rezultatas1 = min2 + (int) Math.round(Math.random() * (max2 - min2));
         System.out.println(rezultatas1);
 
-        //-----------------------
+
         // 1.
+        System.out.println("=============== uzd 1. =================");
 
         String firstName = "Liga";
         String lastName = "Paleckiene";
@@ -39,9 +42,10 @@ public class Main {
         int currentYear = 2023;
 
         System.out.println("Aš esu " + firstName + " " + lastName + ". " + "Man yra " + (currentYear - yearOfBirth) + " metai.");
-        System.out.println("=============== uzd 2. =================");
+
 
         // 2.
+        System.out.println("=============== uzd 2. =================");
         int min = 0;
         int max = 4;
 
@@ -53,13 +57,13 @@ public class Main {
         System.out.println("pirmas random skaicius yra " + randomNumber1);
         System.out.println("antras random skaicius yra " + randomNumber2);
 
-        if( randomNumber1 > 0 && randomNumber2 != 0) {
+        if (randomNumber1 > 0 && randomNumber2 != 0) {
             if (randomNumber1 < randomNumber2) {
                 System.out.println(Math.round((double) randomNumber2 / randomNumber1 * 100) / 100.0);// 4 / 3 = 1; 4.0 / 3 = 1.33333333333333333
             } else {
                 System.out.println(Math.round((double) randomNumber1 / randomNumber2 * 100) / 100.0);
             }
-        }else{
+        } else {
             System.out.println("dalyba is nulio negalima");
         }
 
@@ -74,15 +78,66 @@ public class Main {
 
         // 3.
 
-        int rndNum1 = Math.random()*26;
-        int rndNum2 = Math.random()*26;
-        int rndNum3 = Math.random()*26;
-        System.out.println("Vidurine reiksme");
-        System.out.println();
+        System.out.println("=============== uzd 3. =================");
+
+        int rndNum1 = (int) (Math.random() * 26); // 14
+        int rndNum2 = (int) (Math.random() * 26); // 10
+        int rndNum3 = (int) (Math.random() * 26); // 18
+        System.out.println(rndNum1 + " " + rndNum2 + " " + rndNum3);
+
+        if (rndNum1 >= rndNum2 && rndNum1 <= rndNum3) {
+            System.out.println("Kintamasis su vidutine reiksme yra " + rndNum1);
+        }
+
+        if (rndNum1 <= rndNum2 && rndNum1 >= rndNum3) {
+            System.out.println("Kintamasis su vidutine reiksme yra " + rndNum1);
+        }
+
+        if (rndNum2 >= rndNum1 && rndNum2 <= rndNum3) {
+            System.out.println("Kintamasis su vidutine reiksme yra " + rndNum2);
+        }
+
+        if (rndNum2 <= rndNum1 && rndNum2 >= rndNum3) {
+            System.out.println("Kintamasis su vidutine reiksme yra " + rndNum2);
+        }
+
+        if (rndNum3 <= rndNum1 && rndNum3 >= rndNum2) {
+            System.out.println("Kintamasis su vidutine reiksme yra " + rndNum3);
+        }
+
+        if (rndNum3 > rndNum1 && rndNum3 < rndNum2) {
+            System.out.println("Kintamasis su vidutine reiksme yra " + rndNum3);
+        }
+
 
         // 4.
+        System.out.println("=============== uzd 4. =================");
+
+        int min4 = 1;
+        int max4 = 10;
+
+        int a = min4 + (int) Math.round(Math.random() * (max4 - min4));
+        int b = min4 + (int) Math.round(Math.random() * (max4 - min4));
+        int c = min4 + (int) Math.round(Math.random() * (max4 - min4));
+
+        if (a + b >= c && b + c >= a && a + c >= b){
+            System.out.println("Trikampis yra galimas.");
+        }
+        else
+            System.out.println("Trikampis negalimas.");
+
 
         // 5.
+        System.out.println("=============== uzd 5. =================");
+
+        int =
+        int =
+        int =
+        int =
+
+
+
+
 
         // 6.
 
@@ -90,16 +145,9 @@ public class Main {
 
         // 8.
 
-        System.out.println();
-
-        int num11 = (int) Math.round(Math.random())*100;
-        int num22 = (int) Math.round(Math.random())*100;
-        int num33 = (int) Math.round(Math.random())*100;
-        System.out.println((num11 + num22 + num33)/3);
 
 
 
-        git config --global user.email "liga.paleckiene@gmail.com"
-        git config --global user.name "LygaP"
+
     }
 }
